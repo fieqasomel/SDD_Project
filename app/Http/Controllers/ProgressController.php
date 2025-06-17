0d3cbe3110e5bf45b922f1e3cf522bb92b486caa
dinatasnim <irdinatasnim32@gmail.com>
2025-06-18 00:51:19 +0800

    Update ProgressController.php

--- app/Http/Controllers/ProgressController.php
+++ app/Http/Controllers/ProgressController.php
@@ -156,4 +156,14 @@ class ProgressController extends Controller
 
     return view('Notification.MCMCAlertsView', compact('alerts'));
    }  
+  
+     public function update(Request $request, $id)
+    {
+    $inquiry = Inquiry::findOrFail($id);
+    $inquiry->status = $request->input('status');
+    $inquiry->save();
+
+    return redirect()->back()->with('success', 'Progress updated successfully.');
+    }
+
 }
